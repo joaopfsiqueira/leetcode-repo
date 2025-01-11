3e4cda016dfa533d2a38fb8e30218dd6f69f12fe
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-11 18:49:37 -0300

    feat: fibonacci_number

---
+++ Dynamic Programming/fibonacci_number.ts
@@ -0,0 +1,19 @@
+function fib(n: number): number {
+	if (n === 0) return 0;
+	if (n === 1) return 1;
+
+	let prev = 0;
+	let curr = 1;
+
+	for (let i = 2; i <= n; i++) {
+		const next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
+
+	return curr;
+}
+
+console.log(fib(2));
+console.log(fib(3));
+console.log(fib(4));
